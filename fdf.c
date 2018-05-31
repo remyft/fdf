1a6bb2cd3c699817472d209ac0ca72a8af4dacff
Remy FONTAINE <rfontain@e3r12p3.42.fr>
2018-05-31 17:30:48 +0200

    Grille presque done !

--- fdf.c
+++ fdf.c
@@ -6,7 +6,7 @@
 /*   By: rfontain <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2018/05/29 22:29:07 by rfontain          #+#    #+#             */
-/*   Updated: 2018/05/30 19:58:14 by rfontain         ###   ########.fr       */
+/*   Updated: 2018/05/31 17:29:15 by rfontain         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -33,7 +33,9 @@ int		deal_key(int key, void *param)
 
 	data = param;
 	if (key == 53)
+	{
 		mlx_destroy_window((*data).mlx, (*data).win);
+	}
 	return (0);
 }
 
@@ -84,17 +86,20 @@ void	ft_putgrid(char **lines, void *param)
 	{
 		y = 50;
 		j = 0;
-		while (lines[i][j] && lines[i][j] == '0')
+		while (lines[i][j])
 		{
-			if (i > 0 && lines[i - 1][j])
-				ft_printseg(x, y, x, y - 10, param);
-			if (lines[i + 1][j])
-				ft_printseg(x, y, x, y + 10, param);
-			if (lines[i][j - 2] && lines[i][j - 2] == '0')
-				ft_printseg(x, y, x - 10, y, param);
-			if (lines[i][j + 2] && lines[i][j - 2] == '0')
-				ft_printseg(x, y, x + 10, y, param);
-			y += 10;
+			if (lines[i][j] == '0')
+			{
+				if (i > 0 && lines[i - 1][j])
+					ft_printseg(x, y, x - 10, y, param);
+				if (lines[i + 1][j])
+					ft_printseg(x, y, x + 10, y, param);
+				if (lines[i][j - 2] && lines[i][j - 2] == '0')
+					ft_printseg(x, y, x, y - 10, param);
+				if (lines[i][j + 2] && lines[i][j - 2] == '0')
+					ft_printseg(x, y, x, y + 10, param);
+				y += 10;
+			}
 			j++;
 		}
 		x += 10;
@@ -109,9 +114,10 @@ int main (int ac, char **av)
 	int y;
 	int fd;
 	int i;
-	char *lines[100];
+	char **lines;
 
 	i = 0;
+	lines = malloc(sizeof(char*) * 100);
 	if (ac == 2)
 	{
 		fd = open(av[1], O_RDONLY);
@@ -129,17 +135,17 @@ int main (int ac, char **av)
 	data.mlx = mlx_init();
 	data.win = mlx_new_window(data.mlx, 420, 420, "42");
 	/*while (y < 315)
-	{
-		x = 105;
-		while (x < 315)
-		{
-			mlx_pixel_put(data.mlx, data.win, x, y, 0XFFFFFF);
-			x++;
-		}
-		y++;
-	}
-	ft_printseg(105, 210, 315, 210, &data);*/
-	ft_putgrid(&lines[0], &data);
+	  {
+	  x = 105;
+	  while (x < 315)
+	  {
+	  mlx_pixel_put(data.mlx, data.win, x, y, 0XFFFFFF);
+	  x++;
+	  }
+	  y++;
+	  }
+	  ft_printseg(105, 210, 315, 210, &data);*/
+	ft_putgrid(lines, &data);
 	mlx_key_hook(data.win, deal_key, &data);
 	mlx_loop(data.mlx);
 }
